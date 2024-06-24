a3660195b0375ce8a0fa4e1926ae512d1aa6ee31
yanggw <yanggw@glodon.com>
2024-06-24 12:57:38 +0800

    feat: add tos and sitemap

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,11 @@
 const Footer = () => {
   return (
-    <div className="w-100 py-3 bg-[#150332] dark:bg-[#121212] dark:border-t-2 border-[#2b2b2b] text-slate-200 text-center mt-[40px] md:mt-[65px] text-sm">
-      Designed & Developed by Md Rajaullah Ansari (@mdrajaullah)
+    <div className="w-full py-3 bg-[#150332] dark:bg-[#121212] dark:border-t-2 border-[#2b2b2b] text-slate-200 text-center mt-[40px] md:mt-[65px] text-sm flex justify-around">
+  <a href="html/privacy.html" target="_blank">Privacy Policy</a>
+  <a href="html/refund.html" target="_blank">Refund Policy</a>
+  <a href="html/tos.html" target="_blank">Terms of Service</a>
+    
+      
     </div>
   );
 };
